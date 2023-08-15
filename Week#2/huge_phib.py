c8ce9cc8aa98f36927fbc57787345536fe0cc3c6
Petro Malyi <pmalyi@gmail.com>
2023-08-15 11:55:39 +0300

    Huge phibonachi numbers

---
+++ Week#2/huge_phib.py
@@ -0,0 +1,35 @@
+# Compute the n-th Fibonacci number modulo m.
+def pisanoPeriod(m):
+    if m <= 1:
+        return m
+    f0 = 0
+    f1 = 1
+    i = 2
+    while i <= 6 * m:
+        fi = (f0 + f1) % m
+        f0 = f1
+        f1 = fi
+        if f0 == 0 and f1 == 1:
+            return i - 1
+        i += 1
+
+def phib(n):
+    if n <= 1:
+        return n
+    f0 = 0
+    f1 = 1
+    i = 2
+    while i <= n:
+        fi = f0 + f1
+        f0 = f1
+        f1 = fi
+        i += 1
+    return fi
+
+
+n, m = map(int, input().split())
+p = pisanoPeriod(m)
+n = n % p
+phib = phib(n)
+res = phib % m
+print(res)
